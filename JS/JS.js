b8dd07c0e20e2e4ccaf377074bed8cc0ecd1181f
Xem <73272762+XemSato@users.noreply.github.com>
2022-09-14 14:22:56 +0200

    Update JS.js

--- JS/JS.js
+++ JS/JS.js
@@ -22,7 +22,7 @@ function Chien(event){
     console.log(l);
 
     let posiX = event.pageX || event.changedTouches[0].pageX;
-    posiX.innerText += "gg";
+    this.posiX.innerText += "heyyy";
     console.log(posiX)
 }
 
