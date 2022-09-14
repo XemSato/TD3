427c041934a279d7b1f08baeeaaad4e98c8c0f2e
Xem <73272762+XemSato@users.noreply.github.com>
2022-09-14 14:11:10 +0200

    test

---
+++ JS/JS.js
@@ -0,0 +1,28 @@
+function chat(){
+    console.log("heyyy");
+}
+
+// document.querySelector("svg").addEventListener(
+//     'click',
+//     chat
+// )
+
+document.querySelector(".accueil>h1").addEventListener(
+    "click",
+    Chien
+)
+
+function Chien(event){
+    console.log("bah alors on se lance ! ");
+    console.log(this);
+    console.log(this.innerText);
+    // var l = event.buttons;
+    // var l = event.pageX;
+    var l = event.offsetX;
+    console.log(l);
+
+    let posiX = event.pageX || event.changedTouches[0].pageX;
+
+    console.log(posiX)
+}
+
